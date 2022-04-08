f4154e9ba4b038456a0c010973ef6d798092e153
Alex Enciso <alexpenciso@gmail.com>
2022-04-07 20:25:30 -0400

    Agregado pagina de usuario

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -62,6 +62,10 @@ const routes: Routes = [
     path: 'settings',
     loadChildren: () => import('./pages/settings/settings.module').then( m => m.SettingsPageModule)
   },
+  {
+    path: 'user',
+    loadChildren: () => import('./pages/user/user.module').then( m => m.UserPageModule)
+  },
 
 
 
