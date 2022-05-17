e7b7de92d197d9db372bf83635d2e6aa51c3ebad
Alex Enciso <alexpenciso@gmail.com>
2022-05-17 19:43:02 -0400

    Ajustes a la visualizacion del estado

--- src/app/pages/task/task.module.ts
+++ src/app/pages/task/task.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { TaskPageRoutingModule } from './task-routing.module';
 
 import { TaskPage } from './task.page';
+import { EstadoTareaModule } from 'src/app/pipes/estado-tarea/estado-tarea.module';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
     IonicModule,
-    TaskPageRoutingModule
+    TaskPageRoutingModule,
+    EstadoTareaModule
   ],
   declarations: [TaskPage]
 })
--- src/app/pages/task/task.page.ts
+++ src/app/pages/task/task.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingController, ToastController, AlertController } from '@ionic/angular';
+import { TareaService } from 'src/app/services/tarea.service';
 
 @Component({
   selector: 'app-task',
@@ -7,32 +11,70 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskPage implements OnInit {
 
-  tarea = {
-    numero: 81470,
-    fecha: '21/12/2021',
-    hora: '14:30',
-    cliente: 'Empresa S.A',
-    ruta: 'R2,6',
-    sistema: 'Sistema 2',
-    estado: 'Activo',
-    // eslint-disable-next-line max-len
-    descripcion: 'Este cliente tiene problemas con su impresora. Verificar si no le falta toner y comprar en caso de que sea necesario',
-  };
+  // tarea = {
+  //   numero: 81470,
+  //   fecha: '21/12/2021',
+  //   hora: '14:30',
+  //   cliente: 'Empresa S.A',
+  //   ruta: 'R2,6',
+  //   sistema: 'Sistema 2',
+  //   estado: 'Activo',
+  //   // eslint-disable-next-line max-len
+  //   descripcion: 'Este cliente tiene problemas con su impresora. Verificar si no le falta toner y comprar en caso de que sea necesario',
+  // };
 
-  constructor() { }
+  tareaForm = this.fb.group({
+    estado: [false],
+    obs: [''],
+  });
+
+  public codigo: string;
+  public tarea: any = null;
+
+  constructor(private activatedRoute: ActivatedRoute,
+    private tareaService: TareaService,
+    private fb: FormBuilder,
+    public loadingController: LoadingController,
+    public toastController: ToastController,
+    private router: Router,
+    public alertController: AlertController) { }
 
   ngOnInit() {
+    this.obtenerTarea();
   }
 
-  colorEstado(estado: string): string {
+  async obtenerTarea() {
+
+
+    this.codigo = this.activatedRoute.snapshot.params.id;
+
+    //caso el codigo sea distinto a 0 hace la consulta a la bd
+    if (this.codigo !== '0') {
+      this.tareaService.getById(this.codigo).subscribe( (data) => {
+        if (data.success) {
+          this.tarea = data.tarea;
+
+          this.tareaForm.setValue({
+            estado : data.tarea.tra_estado === 'A' ,
+            obs: data.tarea.tra_obs,
+          });
+        }else{
+          this.tarea = null;
+        }
+
+      });
+    }
+
+  }
+
+  colorEstado(estado: string): string{
     switch (estado) {
-      case 'Activo':
+      case 'A':
         return 'success';
-      case 'Cerrado':
-        return 'danger';
+      case 'F':
+          return 'danger';
       default:
         return 'light';
     }
-
   }
 }
