0e085f65e2075ab116bfc3572719242c3a768b1f
Alex Enciso <alexpenciso@gmail.com>
2022-05-27 15:05:57 -0400

    Agregado modificacion de nombre de empresa

--- src/app/pages/settings/settings.page.ts
+++ src/app/pages/settings/settings.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { Storage } from '@capacitor/storage';
+import { LoadingController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-settings',
@@ -7,9 +10,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPage implements OnInit {
 
-  constructor() { }
+  configForm = this.fb.group({
+    nombre: ['', Validators.required],
+  });
+
+
+  constructor(private fb: FormBuilder,
+    public loadingController: LoadingController,
+    public toastController: ToastController,) { }
 
   ngOnInit() {
+  }
+
+  ionViewWillEnter(){
+    this.obtenerConfiguraciones();
+  }
+
+  async obtenerConfiguraciones(){
+    const token = await Storage.get({key: 'empresa-nombre'});
+    this.configForm.setValue({
+      nombre: token.value,
+    });
+  }
+
+  async guardarConfiguraciones(){
+    const loading = await this.loadingController.create({
+      message: 'Guardando...',
+    });
+    await loading.present();
+    const config = this.configForm.value;
+    await Storage.set({key: 'empresa-nombre', value: config.nombre});
+    this.obtenerConfiguraciones();
+    await loading.dismiss();
+    const toast = await this.toastController.create({
+      message: 'Configuraciones Guardadas!!!',
+      duration: 2000
+    });
+    await toast.present();
   }
 
 }
